23b3009cbf5962ad21c971fa26f2974dbb5941f3
Sean L <leonardmsean@gmail.com>
2023-05-07 19:47:23 -0400

    finished api route maybe

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -10,4 +10,16 @@ module.exports = (app) => {
 
 app.post("/api/notes", (req, res) => {
   let db = fs.readFileSync("db/db.json");
+
+  db = JSON.parse(db);
+  res.json(db);
+  let newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuid(),
+  };
+
+  db.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(db));
+  res.json(db);
 });
