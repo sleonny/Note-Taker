978a391817a21fe8d02b4e252f0d7ced54f19f4a
Sean L <leonardmsean@gmail.com>
2023-05-31 18:43:50 -0400

    trying new api route methods

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,56 +1,11 @@
-const path = require("path");
-const fs = require("fs");
-const uuid = require("uuid");
-const express = require("express");
-const app = express();
+const app = require('express').Router;
+const fs = require('fs');
+const uuid = require('uuid');
 
-app.use(express.json()); // Add this line to parse JSON data in the request body
-
-app.get("/notes", (req, res) => {
-  fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res
-        .status(500)
-        .json({ error: "Failed to read notes from the file." });
-    }
+app.get('/notes', (req, res) => {
+  fs.readFile('../db/db.json', 'utf-8', (err, data) => {
     const notes = JSON.parse(data);
     res.json(notes);
   });
 });
 
-app.post("/notes", (req, res) => {
-  fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res
-        .status(500)
-        .json({ error: "Failed to read notes from the file." });
-    }
-    const notes = JSON.parse(data);
-
-    const newNote = {
-      title: req.body.title,
-      text: req.body.text,
-      id: uuid.v4(),
-    };
-
-    notes.push(newNote);
-
-    fs.writeFile(
-      path.join(__dirname, "../db/db.json"),
-      JSON.stringify(notes),
-      (err) => {
-        if (err) {
-          console.error(err);
-          return res
-            .status(500)
-            .json({ error: "Failed to write notes to the file." });
-        }
-        res.json(notes);
-      }
-    );
-  });
-});
-
-module.exports = app;
