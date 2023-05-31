14025f92606239bceae96e00227e9de10097dbe5
Sean L <leonardmsean@gmail.com>
2023-05-31 14:00:13 -0400

    got href issue fixed, don't change starter code

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 
 module.exports = app;
