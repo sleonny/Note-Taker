bf2a06084db3e9e223b892e762f6d9c737899d6c
Sean L <leonardmsean@gmail.com>
2023-05-31 18:55:58 -0400

    still working on routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,11 +1,59 @@
-const app = require('express').Router;
-const fs = require('fs');
-const uuid = require('uuid');
+// const app = require("express").Router();
+// const fs = require("fs");
+// const { stringify } = require("querystring");
+// const uuid = require("uuid");
 
-app.get('/notes', (req, res) => {
-  fs.readFile('../db/db.json', 'utf-8', (err, data) => {
+// app.get("/notes", (req, res) => {
+//   fs.readFile("../db/db.json", "utf-8", (err, data) => {
+//     const notes = JSON.parse(data);
+//     res.json(notes);
+//   });
+// });
+
+// app.post("/notes", (req, res) => {
+//   fs.readFile("../db/db.json", "utf-8", (err, data) => {
+//     const notes = JSON.parse(data);
+//     const { title, text } = req.body;
+//     const newNotes = {
+//       title,
+//       text,
+//       id: uuid(),
+//     };
+//     notes.push(newNotes);
+//     fs.writeFile("../db/db.json", JSON.stringify(notes), (err, data) => {
+//       res.json(newNotes);
+//     });
+//   });
+// });
+
+const express = require("express");
+const fs = require("fs");
+const { stringify } = require("querystring");
+const uuid = require("uuid");
+
+const router = express.Router();
+
+router.get("/notes", (req, res) => {
+  fs.readFile("../db/db.json", "utf-8", (err, data) => {
     const notes = JSON.parse(data);
     res.json(notes);
   });
 });
 
+router.post("/notes", (req, res) => {
+  fs.readFile("../db/db.json", "utf-8", (err, data) => {
+    const notes = JSON.parse(data);
+    const { title, text } = req.body;
+    const newNotes = {
+      title,
+      text,
+      id: uuid(),
+    };
+    notes.push(newNotes);
+    fs.writeFile("../db/db.json", JSON.stringify(notes), (err, data) => {
+      res.json(newNotes);
+    });
+  });
+});
+
+module.exports = router;
